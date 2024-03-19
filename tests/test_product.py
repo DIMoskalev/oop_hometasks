491542c15fc0c9a9454c75f801a1dbe261ce4ee2
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-19 21:47:48 +0300

    add tests: test_add_new_product and test_display_price

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,22 @@
+from src.product import Product
+
+
 def test_init_product(new_product):
     assert new_product.name == 'Samsung Galaxy C23 Ultra'
     assert new_product.description == '256GB, Серый цвет, 200MP камера'
     assert new_product.price == 180000.0
     assert new_product.quantity == 5
+
+
+def test_add_new_product(dict_with_product):
+    assert Product.add_new_product(dict_with_product).name == "Samsung Galaxy C23 Ultra"
+
+
+def test_display_price(new_product):
+    assert new_product.display_price == 180_000
+    # assert new_product.display_price(1) == 'Введена некорректная цена'
+    new_product.display_price = 100
+    assert new_product.display_price == 100
+    new_product.display_price = 0
+    assert new_product.display_price == 100
+
