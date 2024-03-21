648b57fce4f1d6ecb122e9c02734cc6ee71044b4
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-21 20:26:30 +0300

    add docstrings

--- src/product.py
+++ src/product.py
@@ -14,14 +14,17 @@ class Product:
 
     @classmethod
     def add_new_product(cls, dict_with_product):
+        """Метод создает товар и возвращает объект, который можно добавлять в список товаров"""
         return cls(**dict_with_product)
 
     @property
     def display_price(self):
+        """Геттер, который отображает цену товара"""
         return self.__price
 
     @display_price.setter
     def display_price(self, new_price):
+        """Сеттер, который задет новое значение цены и проверяет, что введена корректная цена (>0)"""
         if new_price <= 0:
             print('Введена некорректная цена')
         else:
