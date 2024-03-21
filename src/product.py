f245dbbc097fc5772e3568c8941e8b2cba96a056
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-21 20:20:26 +0300

    refactoring methods: add_new_product, display_price and __repr__

--- src/product.py
+++ src/product.py
@@ -9,30 +9,26 @@ class Product:
         """Метод инициализации экземпляра класса"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
     def add_new_product(cls, dict_with_product):
-        name = dict_with_product['name']
-        description = dict_with_product['description']
-        price = dict_with_product['price']
-        quantity = dict_with_product['quantity']
-        return cls(name, description, price, quantity)
+        return cls(**dict_with_product)
 
     @property
     def display_price(self):
-        return self.price
+        return self.__price
 
     @display_price.setter
     def display_price(self, new_price):
         if new_price <= 0:
             print('Введена некорректная цена')
         else:
-            self.price = new_price
+            self.__price = new_price
 
     def __repr__(self):
         return (f"\nНазвание: {self.name}\n"
                 f"Описание: {self.description}\n"
-                f"Цена: {self.price}\n"
+                f"Цена: {self.__price}\n"
                 f"Количество в наличии: {self.quantity}\n")
