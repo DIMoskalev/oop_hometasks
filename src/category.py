a17015b3920fa6a96de10f7d0227648c0cff0ed1
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-22 00:04:05 +0300

    add magic methods __str__ and __len__

--- src/category.py
+++ src/category.py
@@ -15,6 +15,18 @@ class Category:
         Category.total_categories_amount += 1
         Category.total_uniq_products = len(self.__products)
 
+    def __str__(self):
+        """Магический метод, для строкового отображения продукта в формате: 'Продукт, 80 руб. Остаток: 15 шт.'"""
+        result = (
+            f'{product.name}, {product.display_price} руб. Остаток: {product.quantity} шт.'
+            for product in self.__products
+        )
+        return "\n".join(result)
+
+    def __len__(self):
+        """Магический метод, который выводит общее число всех продуктов на складе"""
+        return f'{self.name}, количество продуктов: {len(self.__products)} шт.'
+
     def add_product(self, new_product):
         """Метод, который принимает на вход объект товара и добавляет его в список"""
         self.__products.append(new_product)
