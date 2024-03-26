3d828186a66e730b7857796d065457196764b904
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-26 23:09:28 +0300

    rework test_str and test_len

--- tests/test_category.py
+++ tests/test_category.py
@@ -23,12 +23,8 @@ def test_display_products(new_category):
 
 
 def test_str(new_category):
-    assert new_category.__str__() == (
-        'Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт.\n'
-        'Iphone 15, 210000.0 руб. Остаток: 8 шт.\n'
-        'Xiaomi Redmi Note 11, 1000.0 руб. Остаток: 14 шт.'
-    )
+    assert new_category.__str__() == 'Смартфоны, количество продуктов: 27 шт.'
 
 
 def test_len(new_category):
-    assert new_category.__len__() == 'Смартфоны, количество продуктов: 3 шт.'
+    assert new_category.__len__() == 27
