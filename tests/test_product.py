d6e7e2d02ae71c106ef2af230a88c499b7a24ae6
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-27 21:14:54 +0300

    add some lines with test by color

--- tests/test_product.py
+++ tests/test_product.py
@@ -6,10 +6,12 @@ def test_init_product(new_product):
     assert new_product.description == '256GB, Серый цвет, 200MP камера'
     assert new_product.display_price == 180000.0
     assert new_product.quantity == 5
+    assert new_product.color == 'Серый'
 
 
 def test_add_new_product(dict_with_product):
     assert Product.add_new_product(dict_with_product).name == "Samsung Galaxy C23 Ultra"
+    assert Product.add_new_product(dict_with_product).color == "Серый"
 
 
 def test_display_price(new_product):
