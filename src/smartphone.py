2d38400ad21502dad342e2bf928dfa32f54c53b0
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-03-28 22:35:17 +0300

    fix bugs PEP 8

--- src/smartphone.py
+++ src/smartphone.py
@@ -3,12 +3,12 @@ from src.product import Product
 
 class Smartphone(Product):
 
-        performance: str
-        model: str
-        amount_of_memory: float
+    performance: str
+    model: str
+    amount_of_memory: float
 
-        def __init__(self, name, description, price, quantity, color, performance, model, amount_of_memory):
-            super().__init__(name, description, price, quantity, color)
-            self.performance = performance
-            self.model = model
-            self.amount_of_memory = amount_of_memory
+    def __init__(self, name, description, price, quantity, color, performance, model, amount_of_memory):
+        super().__init__(name, description, price, quantity, color)
+        self.performance = performance
+        self.model = model
+        self.amount_of_memory = amount_of_memory
